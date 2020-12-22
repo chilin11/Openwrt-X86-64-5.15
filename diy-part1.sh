4f957e5754566d654f2632989d59ad210cccaa2d
chilin11 <62920850+chilin11@users.noreply.github.com>
2020-12-22 10:28:00 -0500

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,6 +10,9 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
+sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=4.19/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_TESTING_PATCHVER:=5.4/KERNEL_TESTING_PATCHVER:=4.19/g' ./target/linux/x86/Makefile
+
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
